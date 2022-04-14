064a3d0123a20fd832a3642e035fe3bd32b886fb
Steven Sharar <stevensharar@yahoo.com>
2022-04-14 12:19:25 -0500

    Add project files.

---
+++ Data/Device.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Data;
+
+public class Device
+{
+    public int Index { get; set; }
+    public string id { get; set; }
+    public string Model { get; set; }
+    public bool Registered { get; set; }
+    public bool On { get; set; }
+    public double Battery { get; set; }
+    public string IpAddress { get; set; }
+    public string Version { get; set; }
+    public string WifiMac { get; set; }
+    public string BtMac { get; set; }
+    public List<string> Application { get; set; }
+    public List<string> Files { get; set; }
+
+
+}
